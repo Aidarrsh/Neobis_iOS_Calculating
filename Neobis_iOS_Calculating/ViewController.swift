f223717304537c848feecca143e5a084059fd451
aidar <sharipov_ai@auca.kg>
2023-05-06 18:47:30 +0600

    Initial Commit

---
+++ Neobis_iOS_Calculating/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Neobis_iOS_Calculating
+//
+//  Created by Айдар Шарипов on 6/5/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
